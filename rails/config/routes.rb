7b391e2c823f798237ea499980a6a82aca680a34
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2023-11-18 14:49:11 +0900

    Add: 記事新規作成、記事更新APIを追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
       namespace :current do
         resource :user, only: [:show]
+        resources :articles, only: [:create, :update]
       end
       resources :articles, only: [:index, :show]
     end
