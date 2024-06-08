5a9db7a54084d85d5319c686fc48318a260df945
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2024-06-08 18:27:32 +0900

    Add: マイ記事の一覧および詳細取得アクションを実装

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
       namespace :current do
         resource :user, only: [:show]
-        resources :articles, only: [:create, :update]
+        resources :articles, only: [:index, :show, :create, :update]
       end
       resources :articles, only: [:index, :show]
     end
